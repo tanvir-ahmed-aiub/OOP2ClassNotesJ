623ab35ff0fd9f33e13db9cdc3c0f79144f1ee3a
taanveerahmeed <taanveerahmeed@gmail.com>
2020-10-21 09:58:17 +0600

    Theory: 2 Class

---
+++ First Project/First Project/MyClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Project
+{
+    class MyClass
+    {
+        static void testc(ref string  a, ref string c) {
+            string t = a;
+            a = c;
+            c = t;
+        }
+        static void Main(string[] args)
+        {
+            string name = "Tanvir";
+            Console.WriteLine("My name is " + name);
+            string a = "ac";
+            string c = "ad";
+            testc( ref a,  ref c);
+            Console.WriteLine(a + " " + c);
+
+           /* for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("Even Number: " + i);
+                }
+                else
+                    Console.WriteLine("Odd Number: " + i);
+
+            }*/
+        }
+    }
+}
---
+++ First Project/First Project/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace First_Project
+{
+    class Program
+    {
+        
+    }
+}
